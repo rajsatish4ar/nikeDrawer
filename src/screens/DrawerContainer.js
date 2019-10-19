514ef6bfecdc4673ee80b58defb1c64dff5e7fbd
Satish Rajbhar <satishr@policybazaar.com>
2019-10-20 03:58:09 +0530

    nike drawer

--- src/screens/DrawerContainer.js
+++ src/screens/DrawerContainer.js
@@ -8,7 +8,6 @@ import {
 } from "react-native";
 import DrawerLayout from "react-native-gesture-handler/DrawerLayout";
 const { height } = Dimensions.get("screen");
-import NikeDashBoard from './NikeDashBoard';
 import Drawer from '../components/Drawer';
 
 const styles = StyleSheet.create({
--- src/screens/NikeHome.js
+++ src/screens/NikeHome.js
@@ -9,5 +9,5 @@ import {
 import DrawerLayout from "react-native-gesture-handler/DrawerLayout";
 const { height } = Dimensions.get("screen");
 import NikeDashBoard from './NikeDashBoard';
-import DrawerContainer from './GestureDrawerContainer';
+import DrawerContainer from './DrawerContainer';
 export default DrawerContainer(NikeDashBoard)
